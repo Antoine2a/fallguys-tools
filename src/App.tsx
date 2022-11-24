be1ade5459de34ee8ce52d78788b5c824b9dbb0b
Antoine2a <antoine.blanquet365@gmail.com>
2022-11-24 16:51:52 +0100

    build: adding eslint, poggers

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import React from 'react';
 import './App.css';
 
 function App() {
     return (
         <>
             <h1 className="text-3xl font-bold underline text-center">Hello world!</h1>
+            <h2>Hello World 2</h2>
         </>
     );
 }
